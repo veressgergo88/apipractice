08258eb708b24fd1d2f778eb1658c78324131ded
veressgergo88 <veress.gergo88@gmail.com>
2023-08-31 23:22:31 +0200

    all get request working

--- backend/script.ts
+++ backend/script.ts
@@ -22,43 +22,80 @@ const movieDataSchema = z.array(
 })
 )
 
+const actorActressSchema = z.array(
+  z.object({
+    actor_actress: z.string()
+  })
+)
+
+const roleSchema = z.array(
+  z.object({
+    role: z.string()
+  })
+)
+
 //----------------------The Full Database--------------------------------------
 server.get("/darkknightcast", async (req: Request, res: Response) => {
   const data = await client.query("SELECT * FROM darkknightcast");
   const result = movieDataSchema.safeParse(data.rows)
 
-  !result.success ? res.sendStatus(400) : res.json(result.data)
+  if(!result.success) 
+    res.sendStatus(400) 
+  else 
+    res.json(result.data)
 });
 
 //--------------------------Request ID-----------------------------------------
-server.get("/darkknightcast/:id", async (req: Request, res: Response) => {
+server.get("/darkknightcast/actoractress/:id", async (req: Request, res: Response) => {
   const id = +req.params.id
   
   const data = await client.query(`SELECT * FROM darkknightcast WHERE id='${id}';`,[]);
   const result = movieDataSchema.safeParse(data.rows)
 
-  !result.success ? res.sendStatus(400) : res.json(result.data)
+  if(!result.success) 
+    res.sendStatus(400) 
+  else 
+    res.json(result.data)
 });
-
 //---------------------------Actor/Actress-------------------------------------
 server.get("/darkknightcast/actoractress", async (req: Request, res: Response) => {
-  const data = await client.query(`SELECT actor_actress FROM darkknightcast";`, []);
-  console.log(data)
- /* const result = movieDataSchema.safeParse(data.rows)
+  const data = await client.query(`SELECT actor_actress FROM darkknightcast;`, []);
+  const result = actorActressSchema.safeParse(data.rows)
 
-  !result.success ? res.sendStatus(400) : res.json(result.data)*/
+  if(!result.success)
+    res.sendStatus(400)
+  else
+    res.json(result.data)
 });
 //-----------------------------------Role-------------------------------------------
-server.get("/darkknightcast/roles", async (req: Request, res: Response) => {
+server.get("/darkknightcast/role", async (req: Request, res: Response) => {
+  const data = await client.query(`SELECT role FROM darkknightcast;`, []);
+  const result = roleSchema.safeParse(data.rows)
 
+  if(!result.success)
+    res.sendStatus(400)
+  else
+    res.json(result.data)
 });
 //---------------------------------Gender: male------------------------------------
 server.get("/darkknightcast/actors", async (req: Request, res: Response) => {
+  const data = await client.query(`SELECT * FROM darkknightcast WHERE gender='male';`,[]);
+  const result = movieDataSchema.safeParse(data.rows)
 
+  if(!result.success) 
+    res.sendStatus(400) 
+  else 
+    res.json(result.data)
 });
 //----------------------------------Gender: female-------------------------------------
 server.get("/darkknightcast/actress", async (req: Request, res: Response) => {
+  const data = await client.query(`SELECT * FROM darkknightcast WHERE gender='female';`,[]);
+  const result = movieDataSchema.safeParse(data.rows)
 
+  if(!result.success) 
+    res.sendStatus(400) 
+  else 
+    res.json(result.data)
 });
 //---------------------------INSERT new Actor/Actress, Role, Gender-----------------------
 server.post("/darkknightcast", async (req: Request, res: Response) => {
